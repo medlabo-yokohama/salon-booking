c726af5020a9ee2c7f8b02558d07e2770be4fac9
medlabo-yokohama <med_labo@yokohama-isen.ac.jp>
2026-03-27 15:18:30 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import BookingCalendarUI from './booking_calendar_ui.jsx'
-import AdminDashboard from './admin_dashboard.jsx'
+import BookingCalendarUI from './booking_calendar_ui_with_notifications.jsx'
+import AdminDashboard from './admin_dashboard_with_notifications.jsx'
+import HistoryDashboard from './history_dashboard.jsx'
 
-// URLに ?admin=true がついていたら管理画面を表示
-const isAdmin = new URLSearchParams(window.location.search).get('admin') === 'true'
+const params = new URLSearchParams(window.location.search)
+const isAdmin = params.get('admin') === 'true'
+const isHistory = params.get('history') === 'true'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {isAdmin ? <AdminDashboard /> : <BookingCalendarUI />}
+    {isHistory ? <HistoryDashboard /> : isAdmin ? <AdminDashboard /> : <BookingCalendarUI />}
   </React.StrictMode>
 )
